8652ca937b8bab5ffab1d7ca44c57b1fcb8a1d90
Nuno Caetano <a21902813@alunos.ulht.pt>
2022-02-18 21:43:47 +0000

    config

--- src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
@@ -1,4 +1,19 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
-class Pessoa {
+import java.util.*
+
+class Pessoa(val nome:String,veiculos: List<Veiculo>,dataDeNascimento:Date,carta: Carta,posicao: Posicao){
+
+    fun comprarVeiculo(veiculo: Veiculo){}
+
+    fun pesquisarVeiculo(identificador : String) : Veiculo?{return null}
+
+    fun venderVeiculo(identificador: String,comprador:Pessoa){}
+
+    fun moverVeiculoPara(identificador: String,x:Int,y:Int){}
+
+    fun temCarta():Boolean {return false}
+
+    fun tirarCarta(){}
+
 }
