036cdc1d6938240fd5e6130ccd07e99670143189
Nuno Caetano <a21902813@alunos.ulht.pt>
2022-02-18 21:54:06 +0000

    config

--- src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
@@ -1,3 +1,13 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
-class Motor(val cavalos:Int,val cilindrada:Int,val ligado:Boolean){}
+class Motor(val cavalos:Int,val cilindrada:Int,val ligado:Boolean):Ligavel{
+    override fun ligar() {
+    }
+
+    override fun desligar() {
+    }
+
+    override fun estaLigado(): Boolean {
+        return false
+    }
+}
